631035de4007c538e5265efab2bd9e7f5d173629
fredostar <dev@frederic-radigoy.fr>
2020-01-25 17:48:45 +0100

    intialisation du projet spring bash pour alimenter la base

---
+++ back-spring-bash/src/main/java/fr/fradigoy/backspringbash/BackSpringBashApplication.java
@@ -0,0 +1,13 @@
+package fr.fradigoy.backspringbash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSpringBashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSpringBashApplication.class, args);
+	}
+
+}
