024c4bc55075af04c95ba30540a7ba498d878e0b
fredostar <dev@frederic-radigoy.fr>
2020-01-25 18:46:09 +0100

    Creation de la classe Post avec l'annotation @JsonIgnoreProperties  utilisée pour indiquer que les propriétés qui ne correspondent pas à la réponse json doivent être ignorées

---
+++ back-spring-bash/src/main/java/fr/fradigoy/backspringbash/domain/Post.java
@@ -0,0 +1,38 @@
+package fr.fradigoy.backspringbash.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public class Post {
+
+    private long id;
+
+    private String title;
+
+    private String body;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
